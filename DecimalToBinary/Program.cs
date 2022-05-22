a5663b0930a3cb66c9eddd79f221d579f6232cba
David Hatfield <davidhatfield@gmjr@gmail.com>
2022-05-22 10:18:22 -0400

    Binary complete

--- DecimalToBinary/Program.cs
+++ DecimalToBinary/Program.cs
@@ -6,7 +6,18 @@ namespace DecimalToBinary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter in a series of decimal values (separated by spaces): ");
+            string userNums = Console.ReadLine(); // collects user numbers
+
+            string[] userNumsString = userNums.Split(" "); // splits numbers 5 1234 67 123
+
+            int[] userNumsInt = new int[userNumsString.Length];
+            for (int i = 0; i < userNumsString.Length; i++)
+            {
+                userNumsInt[i] = int.Parse(userNumsString[i]);
+                Console.WriteLine(userNumsInt[i] + " in binary is " + Convert.ToString(userNumsInt[i], 2));
+            }
+
         }
 
     }
